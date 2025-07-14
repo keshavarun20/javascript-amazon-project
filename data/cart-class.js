4cd899e5f4b7ee470cfc21f397c9e5294bb69db5
keshavarun20 <keshavarun1220@gmail.com>
2025-07-14 11:01:48 +0530

    Add Cart class: implement methods for adding, deleting, updating items, and managing local storage

---
+++ data/cart-class.js
@@ -0,0 +1,74 @@
+class Cart {
+  cartItems = [];
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.cartItems = this.loadCart();
+  }
+
+  addToCart(productId, selectedQuantity) {
+    const matchingItem = this.cartItems.find(
+      (cartItem) => cartItem.productId === productId
+    );
+
+    if (matchingItem) {
+      matchingItem.quantity += selectedQuantity;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: selectedQuantity,
+        deliveryOptionId: "1",
+      });
+    }
+
+    this.saveCart();
+  }
+
+  deleteFromCart(productId) {
+    const newCart = this.cartItems.filter(
+      (cartItem) => cartItem.productId !== productId
+    );
+
+    this.cartItems.length = 0;
+    this.cartItems.push(...newCart);
+
+    this.saveCart();
+  }
+
+  updateTheCart(productId, updatedQuantity) {
+    const matchingItem = this.cartItems.find(
+      (cartItem) => cartItem.productId === productId
+    );
+
+    if (matchingItem) {
+      matchingItem.quantity = updatedQuantity;
+      this.saveCart();
+    }
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    const matchingItem = this.cartItems.find(
+      (cartItem) => cartItem.productId === productId
+    );
+
+    if (matchingItem) {
+      matchingItem.deliveryOptionId = deliveryOptionId;
+      this.saveCart();
+    }
+  }
+
+  loadCart() {
+    return JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+  }
+
+  saveCart() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+}
+
+const cart = new Cart("cart-oop");
+const businessCart = new Cart("cart-business");
+
+console.log(cart);
+console.log(businessCart); 
